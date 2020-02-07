80d8f04ab0d3d5d3a5f76f666b14a0afd4b85643
Bruno Muciño <mucinoab@gmail.com>
2020-02-07 14:26:15 -0600

    graficas y newton

--- inicio/views.py
+++ inicio/views.py
@@ -5,26 +5,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import io, urllib, base64
-#from random import randint
+from random import randint
 
 def home_view(request):
-   # plt.rcParams['axes.facecolor'] = '#004c3f'
-
-    t = np.arange(-6.0, 6.0, .5)
-    s = 3*t**2-10
-    fig, ax = plt.subplots()
 
 
-    ax.plot(t, s, label='f(x)')
-    ax.set(xlabel='time (s)', ylabel='voltage (mV)', title='About as simple as it gets, folks')
-    ax.grid()
-    ax.vlines(1.82, -10, 10, color = 'r', label = 'Corte con eje x')
-
-
-    plt.legend(loc='best')
-
-    ''' 
-    fig = plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+   # fig = plt.scatter(x, y, s=area, c=colors, alpha=0.5)
 
 
     # Compute areas and colors
@@ -40,13 +26,13 @@ def home_view(request):
     ax = fig.add_subplot(111, projection='polar')
     c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
 
-    '''
+
 
     buf = io.BytesIO()
 
     #plt.savefig(buf, format='png')
 
-    fig.savefig(buf, format='png')  # dpi = 300
+    fig.savefig(buf, format='png', bbox_inches='tight')  # dpi = 300
     buf.seek(0)
     string = base64.b64encode(buf.read())
     uri = 'data:image/png;base64,' + urllib.parse.quote(string)
